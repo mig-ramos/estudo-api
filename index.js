c1178145b78218194d6c522e1547a997f55bf23d
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-03-14 00:41:12 -0300

    Programado os codigos de status

--- index.js
+++ index.js
@@ -15,14 +15,19 @@ app.post('/createproduct', (req, res) => {
     const name = req.body.name
     const price = req.body.price
 
+    if (!name) {
+        res.status(422).json({ message: 'O campo nome é obrigatório!' })
+        return
+    }
+
     console.log(name)
     console.log(price)
 
-    res.json({message: `O produto ${name} foi criado com sucesso!`})
+    res.status(201).json({message: `O produto ${name} foi criado com sucesso!`})
 })
 
 app.get('/', (req, res) => {
-    res.json({message: 'Primeira rota criada com sucesso!'})
+    res.status(200).json({message: 'Primeira rota criada com sucesso!'})
 })
 
 app.listen(3000)
